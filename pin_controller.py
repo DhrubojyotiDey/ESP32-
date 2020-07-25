77e1ecb03e5225394b0b718098315f2c6643ec59
DhrubojyotiDey <47218753+DhrubojyotiDey@users.noreply.github.com>
2020-07-25 10:30:24 +0530

    Update pin_controller.py
    Final def function for pin_control.

--- pin_controller.py
+++ pin_controller.py
@@ -1,19 +1,19 @@
-from machine import Pin
-a=set()
-while True:
-    o=int(input('Enter pin number: '))
-    
-    if o not in a:
-        a.add(o)
-        po= Pin(o, Pin.OUT)
+def pin_control(o):                 # defining a function in this line.
+    from machine import Pin         # The function creates a set and then stores a number for each pin.
+    po= Pin(o, Pin.OUT)             # If the same number is repeated the 2nd time, the function removes the number
+    if o not in a:                  # from the set. The idea is that the number stored in the set will activate
+        a.add(o)                    # its respective light.
         po.on()
+        print(a)
         print('Pin', o, 'is active.')
         print('Press again to inactive')
     else:
-            
         if o in a:
-            po= Pin(o, Pin.OUT)
             po.off()
             a.remove(o)
+            print(a)
         print('Pin', o, 'is inactive.')
-    
+a=set()
+while True:
+    o=int(input('Enter pin number: '))
+    (pin_control(o))
