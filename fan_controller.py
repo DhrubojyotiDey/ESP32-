dc4fcd1382bd370426cf3fc243162d856e3b4c72
DhrubojyotiDey <47218753+DhrubojyotiDey@users.noreply.github.com>
2021-05-11 19:45:24 +0530

    Update fan_controller.py
    just a minor update to exit loop in a controlled manner.

--- fan_controller.py
+++ fan_controller.py
@@ -1,6 +1,5 @@
 from machine import Pin, PWM
 from time import sleep
-p1 = Pin(32, Pin.OUT)
 
 frequency = 5000
 
@@ -8,12 +7,17 @@ led = PWM(Pin(33), frequency)
 
 while True:
     a=int(input("speed: 1-10: "))
-    if a!=10:
-        led.duty(a*100)
-        sleep(0.005)
-    else:
-        led.duty(1023)
-        sleep(0.005)
-
+    if a not in range (11):
+        led.duty(0)
+        print('fan off')
+        
+    else:        
+        if a!=10:
+            led.duty(a*100)
+            sleep(0.005)
+        else:
+            led.duty(1023)
+            sleep(0.005)
+        
 
 
